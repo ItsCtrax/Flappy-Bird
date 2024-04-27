d25b71de3084b607f7be782b0a75fd7ebba23da5
KPOKC <113797883+ItsCtrax@users.noreply.github.com>
2024-04-27 23:34:08 +0300

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pygame
 from random import *
 
@@ -6,6 +8,9 @@ HEIGHT = 480
 GAP = 100
 PIPE_HEIGHT = 150
 GROUND_HEIGHT = 80
+score = 8
+speed = 3
+shield = False
 
 # Images
 img_back = "bg.png"
@@ -32,13 +37,17 @@ background = pygame.transform.scale(pygame.image.load(img_back), (WIDTH, HEIGHT)
 ground = pygame.image.load(img_ground)
 ground_rect = ground.get_rect(midtop = (WIDTH // 2, HEIGHT - GROUND_HEIGHT))
 
+
 class Bird(pygame.sprite.Sprite):
     def __init__(self, img, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.images = {
             "up": pygame.image.load(img_bird_up),
             "down": pygame.image.load(img_bird_down),
-            "normal": pygame.image.load(img_bird)
+            "normal": pygame.image.load(img_bird),
+            "up_shield": pygame.image.load(img_bird_up_shield),
+            "down_shield": pygame.image.load(img_bird_down_shield),
+            "normal_shield": pygame.image.load(img_bird_shield)
         }
         self.image = self.images["normal"]
         self.rect = self.image.get_rect(center=(x, y))
@@ -58,6 +67,14 @@ class Bird(pygame.sprite.Sprite):
         else:
             self.image = self.images["normal"]
 
+        if shield==True:
+            if self.y_speed < 0:
+                self.image = self.images["up_shield"]
+            elif self.y_speed > 0:
+                self.image = self.images["down_shield"]
+            else:
+                self.image = self.images["normal_shield"]
+
     def jump(self):
         self.y_speed = -8
 
@@ -66,11 +83,12 @@ class Pipe(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect(midbottom=(x, y)) if is_top else self.image.get_rect(midtop=(x, y))
-        self.speed = 3
+        self.speed = speed
+        self.is_scored = False
 
     def update(self):
         self.rect.centerx -= self.speed
-        if self.rect.right < 0:
+        if self.rect.right < 0 and not self.is_scored:
             self.kill()
 
 class Life(pygame.sprite.Sprite):
@@ -85,6 +103,18 @@ class FirstKid(pygame.sprite.Sprite):
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = 2
+
+    def update(self):
+        self.rect.centerx -= self.speed
+        if self.rect.right < 0:
+            self.kill()
+
+class Shield(pygame.sprite.Sprite):
+    def __init__(self, img, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect(center=(x, y))
+        self.speed = 3
 
     def update(self):
         self.rect.centerx -= self.speed
@@ -109,6 +139,7 @@ for i in range(3):
     lives.add(life)
 
 first_kids = pygame.sprite.Group()
+shields = pygame.sprite.Group()
 
 run = True
 while run:
@@ -118,6 +149,9 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird.jump()
+                print(score)
+            if event.key == pygame.K_q:
+                score -= 1
 
     if not run:
         break
@@ -127,6 +161,7 @@ while run:
     bird.update()
     lives.update()
     first_kids.update()
+    shields.update()
     pipes.update()
 
     window.blit(bird.image, bird.rect)
@@ -137,12 +172,34 @@ while run:
     for life in lives:
         window.blit(life.image, life.rect)
 
-    if len(pipes) % 5 == 0 and len(first_kids) == 0:
-        first_kid = FirstKid(img_first_kid, 600, 385)  # <--- changed
-        first_kids.add(first_kid)
+    if len(pipes) % 5 == 0:
+        score -= 1
+    if score == 0:
+        score += 8
+        speed += 1
+
+    if score == 6:
+        if len(first_kids) == 0:
+            first_kid = FirstKid(img_first_kid, 600, 385)  # <--- changed
+            first_kids.add(first_kid)
+    if score == 4:
+        if len(shields) == 0:
+            shield = Shield(img_shield, 600, 385)  # <--- changed
+            shields.add(shield)
+    if score == 2:
+        if len(first_kids) == 0:
+            first_kid = FirstKid(img_first_kid, 600, 385)  # <--- changed
+            first_kids.add(first_kid)
+    if score == 0:
+        if len(first_kids) == 0:
+            first_kid = FirstKid(img_first_kid, 600, 385)  # <--- changed
+            first_kids.add(first_kid)
 
     for first_kid in first_kids:
         window.blit(first_kid.image, first_kid.rect)
+
+    for shield in shields:
+        window.blit(shield.image, shield.rect)
 
     if pygame.sprite.spritecollideany(bird, first_kids):
         if len(lives) < 3:
@@ -150,11 +207,19 @@ while run:
             lives.add(life)
         first_kids.empty()
 
+    if pygame.sprite.spritecollideany(bird, shields):
+        shield = True
+        shields.empty()
+
     window.blit(ground, ground_rect)
 
     collided_pipes = pygame.sprite.spritecollide(bird, pipes, False)
     if collided_pipes:
-        if len(lives) > 1:
+        if shield:
+            for pipe in collided_pipes:
+                pipe.kill()
+            shield = False
+        elif len(lives) > 1:
             lives.sprites()[-1].kill()
             for pipe in collided_pipes:
                 pipe.kill()
@@ -165,6 +230,6 @@ while run:
         spawn_pipes()
 
     pygame.display.update()
-    pygame.time.Clock().tick(120)
+    pygame.time.Clock().tick(90)
 
 pygame.quit()
